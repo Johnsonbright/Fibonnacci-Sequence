3b5d7f832ea758d6b0d00746610dfb07fc9ed258
Ifechukwu Johnson Uzoma <johnsonbright101@gmail.com>
2025-02-07 21:46:47 +0100

    fib (10)

--- index.js
+++ index.js
@@ -3,10 +3,11 @@
 function fibonacci(n) {
     const fib = [0,1]
     for(let i = 2; i < n; i++) {
-        fib[i] = fib[i-1] + fib[i-2]
+        fib[i] = fib[i-1] + fib[i-2] // the loop is a O(n) linear time complexity
     }
 return fib
 }
 console.log(fibonacci(2)) //[0,1]
 console.log(fibonacci(3)) //[0,1,1]
 console.log(fibonacci(7)) //[0,1,1,2,3,5,8]
+console.log(fibonacci(10))
